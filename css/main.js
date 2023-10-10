726db874697a607b481a643a257798e3cde1f8df
Jaille Paula <jaille.paula123@gmail.com>
2023-10-10 16:05:45 -0300

    first commit

---
+++ css/main.js
@@ -0,0 +1,77 @@
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+    this.emailInput = document.querySelector("[name='email']"); // Adicionado para referenciar o campo de email
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+
+  onSubmission(event) {
+    event.preventDefault();
+    event.target.disabled = true;
+    event.target.innerText = "Enviando...";
+    
+    // Verificar se o campo de email está vazio
+    if (this.emailInput.value.trim() === "") {
+      alert("O campo de email é obrigatório.");
+      event.target.disabled = false;
+      event.target.innerText = "Enviar";
+      return;
+    }
+  }
+
+  async sendForm(event) {
+    try {
+      this.onSubmission(event);
+      if (this.emailInput.value.trim() === "") {
+        return; // Não envie o formulário se o campo de email estiver vazio
+      }
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+      this.displaySuccess();
+    } catch (error) {
+      this.displayError();
+      throw new Error(error);
+    }
+  }
+
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h1 class='success'>Mensagem enviada!</h1>",
+  error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+});
+formSubmit.init();
